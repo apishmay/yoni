d3bb63026251341ced49c6d095b97310681a69cf
apishmay <apishmay@gmail.com>
2020-11-04 16:04:08 +0800

    回話測試

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ bot.on('message', function(event) {
         console.log('error');
       });    
   });
-  
-  setTimeout(function(){
-    var userId = '使用者 ID';
-    var sendMsg = '要發送的文字';
-    bot.push(userId,sendMsg);
-    console.log('send: '+sendMsg);
-    },5000);
+
+setTimeout(function(){
+  var userId = '使用者 ID';
+  var sendMsg = '要發送的文字';
+  bot.push(userId,sendMsg);
+  console.log('send: '+sendMsg);
+},5000);
 
     // bot.on('message', function(event) {
     //   if (event.message.type == 'text') {
