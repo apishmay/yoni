20869c326e49bc20ddfd0653b3f732d70a4be804
apishmay <apishmay@gmail.com>
2020-11-05 16:07:19 +0800

    回話測試

--- index.js
+++ index.js
@@ -27,21 +27,22 @@ bot.on('message', function(event) {
       .catch(function(error) {
         // error 
         console.log('error');
-      });    
-  });
-
-setTimeout(function(){
+      });
+      
+    setTimeout(function(){
   var userId = '使用者 ID';
   var sendMsg = '要發送的文字';
   bot.push(userId,sendMsg);
   console.log('send: '+sendMsg);
 },5000);
+  
+  });
+
 
 function countSecond() 
-{　 
-  setTimeout("countSecond()", 1000);
-  var sendMsg = '要發送的文字';
-  bot.push(sendMsg);
+{　
+  var sendMsg = '要發送的文字'; 
+  setTimeout(bot.push(sendMsg), 1000);
 }
     // bot.on('message', function(event) {
     //   if (event.message.type == 'text') {
